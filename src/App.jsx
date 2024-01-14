15701a1fcc285565bf631e219fb9e494219efea5
mibri-hub <mihalka.gabi@gmail.com>
2024-01-14 17:53:10 +0100

    crud op

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,25 @@ import React,{ useEffect, useState } from 'react'
 
 import './App.css'
 import { Todos } from './components/Todos'
+import { Login } from './components/login'
+import { getCurrentUser, signOutUser } from './utils'
+import LogoutIcon from '@mui/icons-material/Logout';
 
 
 function App() {
   const [user,setUser]=useState(null)
+  getCurrentUser(setUser)
 
-
-
-
+  console.log(user);
   return (
-    
-      <div>
+      <div className="app">
      <h1>My Todo</h1>
-     <Todos/>
-      </div>
-      
+     {user && <LogoutIcon 
+     sx={{width:"100%",cursor:"pointer",color:"blue"}}
+     onClick={()=>signOutUser()}
+     />}
+     {user==null ? <Login/> : <Todos/>}
+      </div>  
   )
 }
 
--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -2,16 +2,18 @@ import React, { useEffect, useState } from 'react'
 import { readTodos } from '../utils'
 import List from '@mui/material/List';
 import { Todo } from './Todo';
+import { AddNewItem } from './AddNewItem';
 
 export const Todos = () => {
     const [todos,setTodos]=useState([])
     useEffect(()=>{
-        readTodos(setTodos)
+        const unsubscribe= readTodos(setTodos)
+        return ()=>unsubscribe
     },[])
-    //console.log(todos);
+    console.log(todos);
   return (
     <div>
-        
+        <AddNewItem/>
         <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
             {todos.map(obj=><Todo key={obj.id} {...obj}/> )}
         </List>
